250d44a55500fdecd2c9ec9057777578f9759e80
Tsuji <geekjumpingcarnival@i.softbank.jp>
2024-10-11 08:42:16 +0900

    parse2.rbの初期状態を反映

---
+++ parse2.rb
@@ -0,0 +1,28 @@
+# この下にコードを追加してください
+
+# ここから下はそのままにしてください
+# デバッグのため一時的に変更するのは構いません
+
+str = <<-EOS
+いちご,200,半額
+さくらんぼ,300,3割引
+EOS
+
+parser = Parser.new(",")
+items = parser.parse(str)
+p items.size #=> 2
+p items.first.price #=> 200
+p items.last.discount #=> "3割引"
+p items.map(&:name) #=> ["いちご", "さくらんぼ"]
+
+str = <<-EOS
+メロン\t400\t1割引
+バナナ\t80\t半額
+EOS
+
+parser = Parser.new("\t")
+items = parser.parse(str)
+p items.size #=> 2
+p items.first.price #=> 400
+p items.last.discount #=> "半額"
+p items.map(&:name) #=> ["メロン", "バナナ"]
